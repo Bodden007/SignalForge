6b9da147b37e401abab73eba9dbc9e831012edf8
Bodden007 <92564748+Bodden007@users.noreply.github.com>
2026-08-09 16:32:27 +0500

    Add Modbus register map

---
+++ Modbus/RegisterMap.cs
@@ -0,0 +1,9 @@
+namespace SignalForge.Modbus;
+
+public static class RegisterMap
+{
+    public const ushort Pressure = 0;
+    public const ushort Temperature = 2;
+    public const ushort Density = 4;
+    public const ushort FlowRate = 6;
+}
